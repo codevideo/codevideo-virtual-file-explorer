cadd67c4746854ba2463ca7842288e664b3f5c24
Chris Frewin <chris.frewin@solve.ch>
2025-03-02 22:15:26 +0100

    added another test for move with absolute file paths

--- __tests__/unit/MoveFiles.ts
+++ __tests__/unit/MoveFiles.ts
@@ -14,6 +14,17 @@ describe("VirtualFileExplorer", () => {
             expect(virtualFileExplorer.getFiles()).toEqual(["/test2.md"]);
             expect(virtualFileExplorer.getLsString()).toEqual("test2.md");
         })
-        
+        it("should move files with absolute paths", () => {
+            // arrange
+            const virtualFileExplorer = new VirtualFileExplorer();
+            virtualFileExplorer.applyAction({"name": "file-explorer-create-file", "value": "~/test.md"});
+
+            // act
+            virtualFileExplorer.applyAction({"name": "file-explorer-move-file", "value": "from:~/test.md;to:~/test2.md"});
+
+            // assert that only test2.md exists
+            expect(virtualFileExplorer.getFiles()).toEqual(["/test2.md"]);
+            expect(virtualFileExplorer.getLsString()).toEqual("test2.md");
+        })
     });
 });
